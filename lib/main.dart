94750cfcdd061b70ec9884ca9ddafa7904328832
ido <ido.hirsh.microsoft@gmail.com>
2020-12-29 13:20:23 +0200

    ver1.0

--- lib/main.dart
+++ lib/main.dart
@@ -62,6 +62,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
